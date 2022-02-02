dc1b0aab4c65d133aa23bdf96bb8fedd2291a0f9
bened-ct <benedict.velasco@datacom.co.nz>
2022-02-02 13:31:47 +1300

    Initial Commit

---
+++ MoodChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodChecker
+//
+//  Created by Benedict Velasco on 2/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
