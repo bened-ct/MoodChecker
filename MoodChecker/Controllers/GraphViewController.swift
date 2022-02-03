05d2d267e17190c560f911e9d3f9c192d7c8b71d
Benedict <benedict.velasco@datacom.co.nz>
2022-02-03 17:37:15 +1300

    Added data record + graph display feature

--- MoodChecker/Controllers/GraphViewController.swift
+++ MoodChecker/Controllers/GraphViewController.swift
@@ -6,24 +6,45 @@
 //
 
 import UIKit
+import Charts
 
-class GraphViewController: UIViewController {
+class GraphViewController: UIViewController, ChartViewDelegate {
 
+    var lineGraph = LineChartView()
+    let weekData = UserDefaults.standard.object(forKey: "ArrayKey") as? [String: Int] ?? [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        lineGraph.delegate = self
+
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
-    /*
-    // MARK: - Navigation
+        lineGraph.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        lineGraph.center = view.center
+        view.addSubview(lineGraph)
+        
+        var entries = [ChartDataEntry]()
+
+        entries.append(ChartDataEntry(x: 0, y: Double(weekData["Monday"] ?? 0)))
+        entries.append(ChartDataEntry(x: 1, y: Double(weekData["Tuesday"] ?? 0)))
+        entries.append(ChartDataEntry(x: 2, y: Double(weekData["Wednesday"] ?? 0)))
+        entries.append(ChartDataEntry(x: 3, y: Double(weekData["Thursday"] ?? 0)))
+        entries.append(ChartDataEntry(x: 4, y: Double(weekData["Friday"] ?? 0)))
+        entries.append(ChartDataEntry(x: 5, y: Double(weekData["Saturday"] ?? 0)))
+        entries.append(ChartDataEntry(x: 6, y: Double(weekData["Sunday"] ?? 0)))
+
+        
+        let set = LineChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.material()
+        let data = LineChartData(dataSet: set)
+        lineGraph.data = data
     }
-    */
 
 }
--- MoodChecker/Controllers/MoodCheckViewController.swift
+++ MoodChecker/Controllers/MoodCheckViewController.swift
@@ -6,24 +6,102 @@
 //
 
 import UIKit
+import Foundation
 
 class MoodCheckViewController: UIViewController {
-
+    
+    var message = "Select a mood" // message displayed on page that gets changed
+    let weekday = Date.getWeekday() // extracts weekday
+    var weekly: [String: Int] = ["Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0] // array that stores data and gets saved in User Default
+    
+    @IBOutlet weak var fineButton: UIButton!
+    @IBOutlet weak var mildButton: UIButton!
+    @IBOutlet weak var tiredButton: UIButton!
+    @IBOutlet weak var stressButton: UIButton!
+    @IBOutlet weak var veryButton: UIButton!
+    @IBOutlet weak var extremeButton: UIButton!
+    @IBOutlet weak var moodDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        showText()
+        
+        // Assigns value tags to respective buttons
+        fineButton.tag = 0
+        mildButton.tag = 1
+        tiredButton.tag = 2
+        stressButton.tag = 3
+        veryButton.tag = 4
+        extremeButton.tag = 5
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Changes the text based on button pressed
+    func showText(){
+        moodDisplay.text = message
     }
-    */
+    
+    // Save the mood's button tag to respective weekday in weekly array
+    func storeMood(value: Int){
+        weekly[weekday] = value
+        
+        // User Defaults used to record the mood pressed for the corresponding weekday
+        let defaults = UserDefaults.standard
+        defaults.set(weekly, forKey: "ArrayKey")
+        defaults.synchronize()
+    }
 
+    @IBAction func moodPressed(_ sender: UIButton) {
+        let titleString = sender.titleLabel?.text
+        
+        message = titleString!
+        showText() //
+        
+        let data = sender.tag
+        storeMood(value: data)
+        
+        // Code used to figure out if data has been saved - comment out to check
+//        print(data) // display tag of button
+        
+//        print(weekday) // display weekday
+        
+//        for (key, value) in weekly {
+//            print(key, value) // prints array to show that data has been saved
+//        }
+        
+    }
+    
+}
+
+// Used to extract the weekday of present day
+extension Date {
+
+ static func getCurrentDate() -> String {
+
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        return dateFormatter.string(from: Date())
+
+    }
+    
+ static func getDayNameBy(stringDate: String) -> String
+    {
+        let df  = DateFormatter()
+        df.dateFormat = "YYYY-MM-dd"
+        let date = df.date(from: stringDate)!
+        df.dateFormat = "EEEE"
+        return df.string(from: date);
+    }
+    
+    static func getWeekday()-> String {
+        let weekday : String
+        
+        weekday = Date.getDayNameBy(stringDate: Date.getCurrentDate())
+        
+        return weekday
+    }
 }
